3eee078e140b73dbbf0acaf30faabf002fbd6947
Connor Mendenhall <ecmendenhall@gmail.com>
2012-09-09 15:19:33 -0700

    Removed old methods from timer script.

--- timer.js
+++ timer.js
@@ -1,5 +1,6 @@
 function Timer() {
     this.time_ticked = 0;
+    this.total_time = 0;
     this.start_time = new Date().getTime();
     this.ticking = false;
     this.tick_timeout = new Number();
@@ -12,13 +13,11 @@ Timer.prototype.tick = function () {
         this.time_ticked = (now - this.start_time) / 1000;
         //console.log("time_ticked: ", this.time_ticked);
         thisObj = this;
-        var tick_timeout = setTimeout(function() { thisObj.tick(); }, 1000);
+        var tick_timeout = setTimeout(function() { thisObj.tick(); }, 
+                                      1000);
         this.tick_timeout = tick_timeout;
         //console.log("timeout ids :", this.tick_timeouts.length);
         //console.log("timeout id: ", tick_timeout);
-    }
-    if (this.time_ticked > this.time_limit) {
-        this.send_redirect_alarm();
     }
 };
 
@@ -26,12 +25,10 @@ Timer.prototype.start = function () {
     if (this.ticking === false) {
         //console.log("start()");
         this.toggle_icon("on");
-        this.get_time_limit();
         this.start_time = new Date().getTime();
         this.ticking = true;
         this.tick();
     }
-        
 };
 
 Timer.prototype.stop = function () {
@@ -41,6 +38,7 @@ Timer.prototype.stop = function () {
     //this.send_time();
     //console.log("clearing timeout id: ", this.tick_timeout);
     clearTimeout(this.tick_timeout);
+    this.total_time = this.total_time + this.time_ticked;
     this.reset();
     //}
 };
@@ -50,25 +48,9 @@ Timer.prototype.reset = function () {
     this.time_ticked = 0;
 };
 
-Timer.prototype.get_time_limit = function () {
-    chrome.extension.sendRequest({get_time: true,
-                                  url: location.hostname},
-        function(response) {
-            if (response.time_limit) {
-                // Set time limit
-                this.time_limit = response.time_limit;
-                }
-        });
-};
-
 Timer.prototype.toggle_icon = function (status) {
     chrome.extension.sendRequest({toggle_icon: status});
 }
-
-Timer.prototype.send_redirect_alarm = function () {
-    chrome.extension.sendRequest({redirect: true});
-}
-
 
 
 function main () {
@@ -105,8 +87,13 @@ function main () {
         port.onMessage.addListener(function(msg) {
             if (msg.send_time == true) {
                 var sendport = chrome.extension.connect();
+                if (timer.ticking) {
+                    var current_time = timer.time_ticked + timer.total_time; 
+                } else {
+                    var current_time = timer.total_time;
+                    }
                 sendport.postMessage({name: "update",
-                                      update: { current_time: timer.time_ticked, 
+                                      update: { current_time: current_time, 
                                                 url: location.hostname}
                                       });
             }
