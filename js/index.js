5211c3b13f59c08b412eb9fec898a1c597ad12d9
Connor Mendenhall <ecmendenhall@gmail.com>
2012-10-04 12:51:34 -0700

    Added events to analytics tracking script.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $("#install-box").click(function(){ chrome.webstore.install() }) 
+    $("#install-box").click(function(){ 
+        chrome.webstore.install();
+        _gaq.push(['_trackEvent', 'Install', 'Extension Install']);
+    }); 
+    $("#donate-button").click(function(){
+        var amount = $("#prependedInput").value;
+        _gaq.push(['_trackEvent', 'Donate', 'Donation', 'Donation', amount]);
+    });
 });
